7472f2093c532e7ac18f61d8a7a768e153df439f
Fernanda Sandoval <luisa.sandoval@commity.co>
2024-08-16 06:53:44 -0500

    TECH-44: initial setup, navbar and page not found

---
+++ src/modules/error/ErrorPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './errorStyles.module.css'
+
+export const ErrorPage = () => {
+  return (
+    <div>
+        <div className={styles.face}>
+	<div className={styles.band}>
+		<div className={styles.red}></div>
+		<div className={styles.white}></div>
+		<div className={styles.blue}></div>
+	</div>
+	<div className={styles.eyes}></div>
+	<div className={styles.dimples}></div>
+	<div className={styles.mouth}></div>
+    </div>
+
+    <h1>Oops! Something went wrong!</h1>
+    </div>
+  )
+}
