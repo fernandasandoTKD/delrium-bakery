7f7ea816dd15826df2fed256b4080df1da3d5cc2
The-greenpanter <judpenaca@unal.edu.co>
2024-08-30 07:07:25 -0500

    TECH-41

--- src/modules/shopping/ShoppingPage.jsx
+++ src/modules/shopping/ShoppingPage.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import styles from './shoppingStyles.module.css'
+import { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import ProductCard from './productModule';
+import { getProducts } from './Products.jsx';
 
-export const ShoppingPage = () => {
+const ShoppingPage = () => {
+  const [products, setProducts] = useState(getProducts());
+
+  const handleDelete = (id) => {
+    setProducts(products.filter(product => product.id !== id));
+  };
   return (
     <div>
       <h1>Shopping Page</h1>
+      <Container>
+        <Row>
+          <Col>
+            <h2>Tu Carrito</h2>
+            <div className="d-flex flex-wrap">
+              {products.map(product => (
+                <ProductCard key={product.id} 
+                product={product}
+                onDelete={handleDelete}
+                />
+              ))}
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
-}
+  );
+};
+
+export default ShoppingPage;
