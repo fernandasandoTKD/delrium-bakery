1b53c1c9213251ed9c6d36661b4b20b939bff176
Fernanda Sandoval <luisa.sandoval@commity.co>
2024-09-20 12:57:25 -0500

    TECH-37: Changes in structure project and annex connection backend and footer

--- src/modules/products/ProductsPage.jsx
+++ src/modules/products/ProductsPage.jsx
@@ -6,7 +6,7 @@ import torta from '../../assets/Products/torta.jpg'
 import tortaGalletas from '../../assets/Products/tortaGalletas.jpg'
 import panChocolate from '../../assets/Products/panChocolate.jpg'
 import artesanal from '../../assets/Products/artesanal.jpg'
-import cookies  from '../../assets/Products/cookies.png'
+import cookies from '../../assets/Products/cookies.png'
 export const ProductsPage = () => {
 
   /* Anexo de lógica para enviar a la sección espeifica  */
@@ -28,7 +28,7 @@ export const ProductsPage = () => {
             <div className="col-lg-6 text-center">
               <h1 className="text-uppercase text-light">DERILIUM</h1>
               <h2 className="text-white-50 mt-2 mb-5">Tradición artesanal en productos de panadería y respostería.</h2>
-              <a className="btn btn-primary" href="#about">Get Started</a>
+              <a className="btn btn-primary" href="#about">Compra acá</a>
             </div>
             <div className="col-lg-4 d-flex justify-content-end">
               <img
@@ -161,35 +161,62 @@ export const ProductsPage = () => {
       <div id="seccionGalletas" className="container d-flex flex-column align-items-center text-center py-5">
         <h1>Personaliza tus galletas</h1>
         <hr className="hr" />
-        <Container className="pt-5">
-          <Row className="justify-content-center">
-            <Col xs={12} sm={12} md={4} lg={4} className="mb-4 d-flex justify-content-center">
-            <Image src={cookies} fluid roundedCircle  />
-            </Col>
-            <Col xs={12} sm={12} md={4} lg={4} className="mb-4 d-flex justify-content-center">
-              <div id={styles.checklist} className='bg-primary bg-gradient text-light'>
-                <input  value="1" name="r" type="checkbox" id="01" />
-                <label for="01">Bread</label>
-                <input value="2" name="r" type="checkbox" id="02" />
-                <label for="02">Cheese</label>
-                <input value="3" name="r" type="checkbox" id="03" />
-                <label for="03">Coffee</label>
-              </div>
-            </Col>
 
-            <Col xs={12} sm={12} md={4} lg={4} className="mb-4 d-flex justify-content-center">
-              <div id={styles.checklist} className='bg-success bg-gradient text-light'>
-                <input  value="4" name="r" type="checkbox" id="04" />
-                <label for="04">Bread</label>
-                <input value="5" name="r" type="checkbox" id="05" />
-                <label for="05">Cheese</label>
-                <input value="6" name="r" type="checkbox" id="06" />
-                <label for="06">Coffee</label>
+        <Container className={styles.container}>
+          <form className="w-100">
+            <div className="row justify-content-center">
+              {/* Columna 1: Categorías */}
+              <div className="col-md-4 p-3 border rounded">
+                <h4>Categorías</h4>
+                <label>
+                  <input type="radio" name="categoria" />
+                  <span>Opción 1</span>
+                </label>
+                <br />
+                <label>
+                  <input type="radio" name="categoria" />
+                  <span>Opción 2</span>
+                </label>
               </div>
-            </Col>
-          </Row>
+
+              {/* Columna 2: Glaseados */}
+              <div className="col-md-4 p-3 border rounded">
+                <h4>Glaseados</h4>
+                <label>
+                  <input type="radio" name="glaseado" />
+                  <span>Glaseado 1</span>
+                </label>
+                <br />
+                <label>
+                  <input type="radio" name="glaseado" />
+                  <span>Glaseado 2</span>
+                </label>
+              </div>
+
+              {/* Columna 3: Tipo de Galleta */}
+              <div className="col-md-4 p-3 border rounded">
+                <h4>Tipo de Galleta</h4>
+                <label>
+                  <input type="radio" name="tipoGalleta" />
+                  <span>Galleta 1</span>
+                </label>
+                <br />
+                <label>
+                  <input type="radio" name="tipoGalleta" />
+                  <span>Galleta 2</span>
+                </label>
+              </div>
+            </div>
+            {/* Botón Personalizar */}
+            <div className="d-flex justify-content-center mt-4">
+              <button type="submit" className="btn btn-success btn-lg">
+                Personalizar
+              </button>
+            </div>
+          </form>
         </Container>
       </div>
+
     </div>
   )
 }
---
+++ src/private/components/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+// src/components/PrivateRoute.js
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const PrivateRoute = ({ element }) => {
+    const { isAuthenticated } = useAuth();
+    return isAuthenticated ? element : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
