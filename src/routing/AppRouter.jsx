204b20800266db9a597f288964fff9100df50b70
The-greenpanter <judpenaca@unal.edu.co>
2024-09-28 18:30:18 -0500

    merge

--- src/routing/AppRouter.jsx
+++ src/routing/AppRouter.jsx
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import logo from '../assets/logo.png';
-import ShoppingPage from "../modules/shopping/ShoppingPage";
-import { UsersPage } from "../modules/users/UsersPage";
-import { Navbar, Nav ,Container} from 'react-bootstrap';
-=======
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
-
 import ShoppingPage from "../modules/shopping/ShoppingPage";
->>>>>>> ba09ffd43a8fa5ef09f30a007117b86f07033eb2
 import { ProductsPage } from "../modules/products/ProductsPage";
 import { ErrorPage } from "../modules/error/ErrorPage";
 import {BlogPage} from "../modules/blog/BlogPage";
@@ -33,60 +24,6 @@ import { AdminView } from "../private/classes/privateAdmin/AdminView";
 
 
 
-<<<<<<< HEAD
-export const AppRouter = () => {
-  const state = useSelector((state) => state); // Log the entire state
-  console.log(state); // Check the structure of the state
-
-  const userPermissions = state.permissions.userPermissions; // Asegúrate de que esta ruta sea correcta
-
-  return (
-    <Router>
-      {/* Menú de Navegación */}
-      <Navbar bg="success" variant="ligth" expand="lg" className="bg-body-tertiary">
-      <Container>
-      <Navbar.Brand href="/products">
-            <img
-              alt=""
-              src={logo}
-              width="50"
-              height="50"
-              className="d-inline-block align-top"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-              <Nav.Link href='/products' className="text-light fs-5">Productos</Nav.Link>
-                {userPermissions.includes('view_users') && ( // Solo muestra si el usuario tiene permiso
-                  <Nav.Link href='/users' className="text-light fs-5">Usuarios</Nav.Link>
-                )}
-                {userPermissions.includes('view_quotes') && ( // Solo muestra si el usuario tiene permiso
-                  <Nav.Link href='/quotes' className="text-light fs-5">Cotizaciones</Nav.Link>
-                )}
-                <Nav.Link href='/shopping' className="text-light fs-5">Compras</Nav.Link>
-                <Nav.Link href='/classes' className="text-light fs-5">Clases</Nav.Link>
-                <Nav.Link href="/about" className="text-light fs-5">Sobre nosotros</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      {/* Configurar rutas */}
-      <Routes>
-          <Route path='/' element={<div className="content"><ProductsPage /></div>}></Route>
-          <Route path='/shopping' element={<div className="content"><ShoppingPage /></div>}></Route>
-          {userPermissions.includes('view_users') && (
-            <Route path='/users' element={<div className="content">< UsersPage/></div>}></Route>
-          )}
-          <Route path='/products' element={<div className="content"><ProductsPage /></div>}></Route>
-          <Route path='/classes' element={<div className="content">< ClassesPages/></div>}></Route>
-          {userPermissions.includes('view_quotes') && (
-            <Route path='/quotes' element={<div className="content">< QuotesPages/></div>}></Route>
-          )}
-          <Route path='*' element={<ErrorPage />}></Route>
-        </Routes>
-    </Router>
-=======
   return (
 
       <AuthProvider>
@@ -118,6 +55,4 @@ export const AppRouter = () => {
      </Router>
       </AuthProvider>
     
->>>>>>> ba09ffd43a8fa5ef09f30a007117b86f07033eb2
   )
-}
