31010d12d28a318a60cc159b85347ee1a5a94ed8
NyuAngel <lvfandino@misena.edu.co>
2024-10-01 18:25:12 -0500

    Get posts ok

--- src/modules/blog/components/Posts.jsx
+++ src/modules/blog/components/Posts.jsx
@@ -1,16 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect} from 'react'
 import PostItem from '../components/PostItem'
-import { DUMMY_POSTS } from '../data'
 import "../css/stylesblog.css"
+import axios from 'axios';
+import { Global } from '../../../helpers/Global';
 
 const Posts = () => {
-  const [posts, setPosts] = useState(DUMMY_POSTS)
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(`${Global.url}post`);
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Error al obtener Posts:', error);
+      }
+    };
+
+    fetchPosts(); // Llamar a la función para obtener las clases al montar el componente
+  }, []);
+
   return (
     
     <section className="posts"> 
       {posts.length > 0 ? <div className="container posts_container">
         {
-            posts.map(({id, thumbnail,category, title, desc, authorID })=> <PostItem  key={id} postID={id} thumbnail={thumbnail} category= {category} title ={title} description={desc} authorID={authorID} />)
+            posts.map(({_id, thumbnail,category, title, description, authorID })=> <PostItem  key={_id} postID={_id} thumbnail={thumbnail} category= {category} title ={title} description={description} authorID={"authorID"} />)
         }
       </div> : <h2 className='center'> No posts founds </h2>}
     </section> 
