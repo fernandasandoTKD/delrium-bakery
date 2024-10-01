a0b38d5deb8128c0e4929cd916a87fd64296c746
NyuAngel <lvfandino@misena.edu.co>
2024-10-01 10:15:16 -0500

    edit y delete only admin ok

--- src/modules/blog/sub_pages/PostDetail.jsx
+++ src/modules/blog/sub_pages/PostDetail.jsx
@@ -2,18 +2,24 @@ import React from 'react'
 import PostAuthor from '../components/PostAuthor'
 import { Link } from 'react-router-dom'
 import Thumbnail from '../images/blog22.jpeg'
-
+import useAuth from '../../../hooks/useAuth'
 
 export const PostDetail = () => {
+  const { auth, logout } = useAuth();
+  console.log("pruebaadmi", auth)
   return (
     <section className= "post-detail">
       <div className="container post-detail__container">
         <div className="post-detail__header">
           <PostAuthor/>
-         {/*  <div className="post-detail__buttons">
-            <Link to={`/posts/werwer/edit`} className='btn sm primary'> Edit</Link>
-            <Link to={`/posts/werwer/delete`} className='btn sm danger'> Delete</Link>
-          </div> */}
+            {(auth && auth.role == "admin") && 
+              <div className="post-detail__buttons">
+              <Link to={`/posts/werwer/edit`} className='btn sm primary'> Edit</Link>
+              <Link to={`/posts/werwer/delete`} className='btn sm danger'> Delete</Link>
+              </div> 
+            }
+         
+
         </div>
         <h1> El pan de chocolate es funcional? </h1>
         <div className="post-detail__thumbnail">
